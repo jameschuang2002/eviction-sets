618747d91b0438aa7b37558eeb2f41215ff8d430
Max Christman <54341372+maxchristman@users.noreply.github.com>
2024-11-18 14:31:59 -0500

    Root isn't needed.

--- test.sh
+++ test.sh
@@ -9,4 +9,4 @@ gcc -std=c99 -O0 -g $compiler_flags -pthread -c lib/eviction.c -o bin/eviction.o
 gcc -std=c99 -O0 -g -c src/test.c -o bin/test.o
 g++ -g -pthread -O0 bin/test.o bin/eviction.o bin/utils.o -o bin/test.out
 
-sudo ./bin/test.out
+./bin/test.out
